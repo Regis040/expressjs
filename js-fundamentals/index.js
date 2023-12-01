d879050f20e673dec4f42d64fa201e28295c773f
Regis JOUAN <regis.jouan@lapiscine.pro>
2023-12-01 10:24:32 +0100

    jour4-1

--- js-fundamentals/index.js
+++ js-fundamentals/index.js
@@ -11,14 +11,15 @@ let myNumber = 12
 
 let myNumber2 = myNumber
 
-
+// chacune de ces variables fait sa vie independdement de l'autre
 
 
 myNumber += 3
+console.log(myNumber2) //va donner 12 et non pas 13
 
 
 
-
+// une variblae c'est deux choses : un nom et une valeur
 
 
 
@@ -46,39 +47,32 @@ myName = "Pauline"
 // ce qui signifie que les 2 variables pointent vers une même valeur.
 
 let user = {
-
     name: "Paul",
-
     age: 35
-
 }
 
+let user2 = { ...user } // on crée un tableau user indépendement du user2
 
-
-
-let user2 = { ...user }
-
-
-
+// équivalent à let user2=user
 
 user.name = "Stéphane"
 
 user2.name = "Victoria"
+//un log des deux user montrera que chauqe tabealu est indépendant
 
 
 
 
 
 
-
-let arr1 = [2, 5, 7]
+let arr1 = [2, 5, 7] //arr1 a pour reférenec le tableau --- il pointe vers
 
 arr1.push(9)
 
 
 
 
-let arr2 = arr1
+let arr2 = arr1 // arr2 pointe vers la même réfernce que arr1
 
 
 
